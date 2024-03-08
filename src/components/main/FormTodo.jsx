3f59fa573d9a5c9255f9aea0d1e0a9fee182ea61
Shweta Sharma <19shwetasharma46@gmail.com>
2024-03-08 15:51:45 +0530

    Design changes in  todo form

--- src/components/main/FormTodo.jsx
+++ src/components/main/FormTodo.jsx
@@ -75,8 +75,8 @@ function FormTodo() {
   return (
     <div className="h-screen overflow-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       {/* <Header /> */}
-      <section className="flex flex-col items-start justify-start py-16 text-white">
-        <div className="w-1/3 h-auto p-6 mx-auto mt-4 text-black bg-white border rounded">
+      <section className="flex flex-col items-start justify-start py-16">
+        <div className="w-1/3 h-auto p-6 mx-auto mt-4 text-white border shadow-xl border-white/80 bg-black/40">
           <form onSubmit={handleSubmit}>
             <div className="p-2 text-left">
               <label htmlFor="assignee" className="text-sm font-bold">Name</label>
@@ -85,8 +85,9 @@ function FormTodo() {
                 value={inputValues.assignee}
                 name="assignee" id="assignee"
                 type="text"
+                required
                 placeholder="Enter Assignee Name"
-                className="w-full p-2 mt-1 text-sm border rounded focus:outline-none focus:ring focus:ring-violet-500"
+                className="w-full p-2 mt-1 text-sm bg-transparent border rounded border-white/40 focus:outline-none focus:ring focus:ring-violet-500"
               />
             </div>
             <div className="p-2 text-left">
@@ -96,8 +97,9 @@ function FormTodo() {
                 value={inputValues.task}
                 name="task" id="task"
                 type="text"
+                required
                 placeholder="Enter Task"
-                className="w-full p-2 mt-1 text-sm border rounded focus:outline-none focus:ring focus:ring-violet-500"
+                className="w-full p-2 mt-1 text-sm bg-transparent border rounded border-white/40 focus:outline-none focus:ring focus:ring-violet-500"
               />
             </div>
             <div className="p-2 text-left">
@@ -106,11 +108,12 @@ function FormTodo() {
                 onChange={handleInputChange}
                 defaultValue={inputValues.priority}
                 name="priority" id="priority"
-                className="w-full p-2 mt-1 text-sm capitalize border rounded focus:outline-none focus:ring focus:ring-violet-500"
+                required
+                className="w-full p-2 mt-1 text-sm capitalize bg-transparent border rounded border-white/40 focus:outline-none focus:ring focus:ring-violet-500"
               >
-                <option value="high">High</option>
-                <option value="mid">Mid</option>
-                <option value="low">Low</option>
+                <option value="high" className="text-black bg-transparent">High</option>
+                <option value="mid" className="text-black bg-transparent">Mid</option>
+                <option value="low" className="text-black bg-transparent">Low</option>
               </select>
             </div>
             <div className="p-2 mt-4">
@@ -124,61 +127,63 @@ function FormTodo() {
           </form>
         </div>
         <div className="w-3/4 mx-auto my-6">
-          <table className="min-w-full">
-            <thead className="border-b bg-slate-50 border-slate-200">
+          <table className="min-w-full shadow-xl bg-black/40">
+            <thead className="border border-slate-200">
               <tr>
-                <th className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                <th className="px-6 py-3 text-sm font-bold text-left text-white ">
                   Assignee Name
                 </th>
-                <th className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                <th className="px-6 py-3 text-sm font-bold text-left text-white ">
                   Task Name
                 </th>
-                <th className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                <th className="px-6 py-3 text-sm font-bold text-left text-white ">
                   Priority Name
                 </th>
-                <th className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                <th className="px-6 py-3 text-sm font-bold text-left text-white ">
                   Status
                 </th>
-                <th className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                <th className="px-6 py-3 text-sm font-bold text-center text-white">
                   Actions
                 </th>
               </tr>
             </thead>
-            <tbody className="border-b bg-slate-50 border-slate-200">
+            <tbody className="border border-t-0 border-slate-200">
               {formData.map(data => (
                 <tr key={data.id}>
                   {editingId === data.id ? 
                     <>
-                      <td className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                      <td className="px-6 py-3 text-sm font-medium text-left text-white">
                         <input
                           type="text"
                           value={assigneeName}
                           onChange={(e) => setAssigneeName(e.target.value)}
                           name="assignee"
+                          className="p-2 bg-transparent border rounded-lg focus:outline-none"
                         />
                       </td>
-                      <td className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                      <td className="px-6 py-3 text-sm font-medium text-left text-white">
                         <input
                           type="text"
                           value={taskName}
                           onChange={(e) => setTaskName(e.target.value)}
                           name="task"
+                          className="p-2 bg-transparent border rounded-lg focus:outline-none"
                         />
                       </td>
-                      <td className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                      <td className="px-6 py-3 text-sm font-medium text-left text-white">
                         <select
-                          className="w-48 p-2 capitalize border rounded-lg outline-0"
+                          className="w-40 p-2 capitalize bg-transparent border rounded-lg focus:outline-none outline-0"
                           name="priority"
                           defaultValue={taskPriority}
                           onChange={(e) => setTaskPriority(e.target.value)}
                           required
                         >
-                          <option value="high">High</option>
-                          <option value="mid">Mid</option>
-                          <option value="low">Low</option>
+                          <option className="text-black bg-transparent" value="high">High</option>
+                          <option className="text-black bg-transparent" value="mid">Mid</option>
+                          <option className="text-black bg-transparent" value="low">Low</option>
                         </select>
                       </td>
-                      <td className="w-56 py-2">
+                      <td className="py-3 pl-6">
                         <label className="flex items-center gap-2 cursor-pointer select-none">
                           <div className="relative flex items-center gap-2">
                             <input
@@ -190,13 +195,13 @@ function FormTodo() {
                             />
                             <div className="h-8 bg-gray-100 rounded-full shadow-lg w-14 peer-checked:bg-gray-200"></div>
                             <div className="absolute w-6 h-6 transition bg-yellow-500 rounded-full top-1 left-1 peer-checked:translate-x-full peer-checked:bg-green-500"></div>
-                            <span className="text-sm font-bold text-yellow-500 peer-checked:text-green-500">
+                            <span className="text-sm text-yellow-300 peer-checked:text-green-300">
                               {data.completed ? "completed" : "In processing"}
                             </span>
                           </div>
                         </label>
                       </td>
-                      <td className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                      <td className="px-6 py-3 text-sm font-medium text-white">
                         <div
                           className="flex items-center justify-center gap-3"
                           onClick={saveEditedValue}
@@ -206,16 +211,16 @@ function FormTodo() {
                       </td>
                     </> :
                     <>
-                      <td className="px-6 py-3 text-sm font-medium text-left text-slate-900">
-                        {data.assignee}
+                      <td className="px-6 py-3 text-sm font-medium text-left text-white">
+                        <h6 className="p-2">{data.assignee}</h6>
                       </td>
-                      <td className="px-6 py-3 text-sm font-medium text-left text-slate-900">
-                        {data.task}
+                      <td className="px-6 py-3 text-sm font-medium text-left text-white">
+                        <h6 className="p-2">{data.task}</h6>
                       </td>
-                      <td className="px-6 py-3 text-sm font-medium text-left capitalize text-slate-900">
-                        {data.priority}
+                      <td className="px-6 py-3 text-sm font-medium text-left text-white capitalize">
+                        <h6 className="p-2">{data.priority}</h6>
                       </td>
-                      <td className="w-56 py-2">
+                      <td className="px-6 py-3">
                         <label className="flex items-center gap-2 select-none">
                           <div className="relative flex items-center gap-2">
                             <input
@@ -227,13 +232,13 @@ function FormTodo() {
                             />
                             <div className="h-8 bg-gray-100 rounded-full shadow-lg w-14 peer-checked:bg-gray-200"></div>
                             <div className="absolute w-6 h-6 transition bg-yellow-500 rounded-full top-1 left-1 peer-checked:translate-x-full peer-checked:bg-green-500"></div>
-                            <span className="text-sm font-bold text-yellow-500 peer-checked:text-green-500">
+                            <span className="text-sm font-bold text-yellow-300 peer-checked:text-green-300">
                               {data.completed ? "completed" : "In processing"}
                             </span>
                           </div>
                         </label>
                       </td>
-                      <td className="px-6 py-3 text-sm font-medium text-left text-slate-900">
+                      <td className="px-6 py-3 text-sm font-medium text-white">
                         <div className="flex items-center justify-center gap-3">
                           <div
                             onClick={() =>
@@ -244,7 +249,6 @@ function FormTodo() {
                                 data.priority
                               )
                             }
-                            className="cursor-pointer "
                           >
                             <i className="cursor-pointer fa-solid fa-pen"></i>
                           </div>
@@ -252,7 +256,7 @@ function FormTodo() {
                             onClick={() => deleteTask(data.id)}
                             className="cursor-pointer"
                           >
-                            <i className="cursor-pointer fa-solid fa-trash"></i>
+                            <i className="fa-solid fa-trash"></i>
                           </div>
                         </div>
                       </td>
